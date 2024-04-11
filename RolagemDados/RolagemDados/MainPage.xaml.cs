afa73422087f4626f4d171ac353b3e5572c67665
Luiza2112 <ritafilhotes4@gmail.com>
2024-04-11 13:17:13 -0300

    :sparkles:feat: Design concluido.

---
+++ RolagemDados/RolagemDados/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace RolagemDados
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRollBtn_Clicked(object sender, EventArgs e)
+        {
+            Random num = new();
+            //int Number = Convert.ToDouble(DiceNumbers.Text);
+
+            //for (int ctr = 1; ctr = Number; ctr++)
+            {
+
+            }
+
+        }
+    }
+}
